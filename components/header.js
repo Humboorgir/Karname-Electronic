1c3e2da5071ccdf5df0e9cd596bf5e5466de7ba2
Humboorgir <hardcraftap@gmail.com>
2023-01-21 14:53:23 +0330

    addedtailwindcss

---
+++ components/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="bg-red-600">
+          <li>ورود دانش آموز</li>
+          <li>ورود معلم</li>
+          <li>پنل ادمین</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
