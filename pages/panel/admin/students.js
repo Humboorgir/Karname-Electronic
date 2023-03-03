09114a239321b83cdc4e6d594eede25f3825a441
Humboorgir <hardcraftap@gmail.com>
2023-03-03 18:34:22 +0330

    fixed internal server error on panel/admin/students

--- pages/panel/admin/students.js
+++ pages/panel/admin/students.js
@@ -13,7 +13,7 @@ const Students = ({ Students }) => {
   );
 };
 export async function getServerSideProps() {
-  let students = await fetch("/api/students", {
+  let students = await fetch(`${process.env.NEXTAUTH_URL}/api/students`, {
     method: "POST",
   });
   let Students = await students.json();
