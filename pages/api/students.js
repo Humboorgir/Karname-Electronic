25ef64c1148702156e3d1de253817dbb8e0c2479
Humboorgir <hardcraftap@gmail.com>
2023-03-04 18:04:30 +0330

    .

--- pages/api/students.js
+++ pages/api/students.js
@@ -2,8 +2,8 @@ import studentModel from "@/models/students";
 import mongoose from "mongoose";
 import mongoConnect from "@/utils/mongoconnect";
 export default async function handler(req, res) {
-  mongoConnect();
   if (req.method !== "POST") return res.send("only post requests are allowed");
+  mongoConnect();
   let data = await studentModel.find({});
   console.log(data);
   res.send(data);
