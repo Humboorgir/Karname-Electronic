680c510d52eed9a69691db67e8d7eae18d20ffee
Humboorgir <hardcraftap@gmail.com>
2023-03-31 21:15:54 +0330

    fixed dynamic admin info not working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import mongoConnect from "@/utils/mongoconnect";
 import managersModel from "@/models/managers";
 export const authOptions = {
   debug: true,
@@ -14,20 +15,27 @@ export const authOptions = {
       type: "credentials",
 
       async authorize(credentials, req) {
-        const storedAdmins = managersModel.find({});
-        const admins = storedAdmins.length
-          ? storedAdmins
-          : [
-              {
-                username: "admin",
-                password: "admin",
-              },
-            ];
-        const user = admins.find(
-          (admin) =>
-            admin.username === credentials.username &&
-            admin.password === credentials.password
-        );
+        const { username, password } = credentials;
+        let defaultAdmins = [
+          {
+            username: "admin",
+            password: "admin",
+          },
+        ];
+        // first, establish connection with the mongodb database.
+        mongoConnect();
+        // then, check to see if any managers are stored in the database.
+        let storedManagers = await managersModel.find({});
+        // if yes, look for someone with the provided information in the database,
+        // if not, use the default 'admin' username and password.
+        let user = storedManagers.length
+          ? await managersModel.find({ username: username, password: password })
+          : defaultAdmins.find(
+              (admin) =>
+                admin.username === username && admin.password === password
+            );
+        // if the username and password provided matches none, it returns an empty Array which we convert to a null variable.
+        if (Array.isArray(user) && !user.length) user = null;
         if (user) {
           return user;
         } else {
