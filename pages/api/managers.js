26b83abf4e3a19758f3f0e0dcbe88745d113aa0a
Humboorgir <hardcraftap@gmail.com>
2023-06-25 00:18:07 +0330

    install daisyui

--- pages/api/managers.js
+++ pages/api/managers.js
@@ -1,7 +1,7 @@
 import managerModel from "@/models/managers";
 import mongoConnect from "@/utils/mongoconnect";
 export default async function handler(req, res) {
-  if (req.method !== "GET") return res.send("only get requests are allowed");
+  if (req.method !== "GET") return;
   mongoConnect();
   let data = await managerModel.find({});
   res.send(data);
