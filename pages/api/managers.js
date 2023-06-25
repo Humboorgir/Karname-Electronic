b5dfb80227591db0f47c602b3b96b9542114eec5
Humboorgir <hardcraftap@gmail.com>
2023-06-25 18:58:55 +0330

    fix /api/managers

--- pages/api/managers.js
+++ pages/api/managers.js
@@ -1,8 +1,8 @@
-import managerModel from "@/models/managers";
-import mongoConnect from "@/utils/mongoconnect";
+import prisma from "@/lib/prisma";
+
 export default async function handler(req, res) {
   if (req.method !== "GET") return;
-  mongoConnect();
-  let data = await managerModel.find({});
-  res.send(data);
+  let data = await prisma.manager.findMany();
+
+  res.json(data);
 }
