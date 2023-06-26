f7094355b771754ad099efc539c0a8aece453d87
Humboorgir <hardcraftap@gmail.com>
2023-06-26 17:41:08 +0330

    handle post requests sent to /api/managers

--- pages/api/managers.js
+++ pages/api/managers.js
@@ -1,13 +1,30 @@
 import prisma from "@/lib/prisma";
 
 export default async function handler(req, res) {
-  let data;
+  const { username, name, image } = req.body;
 
+  // handling get requests
   if (req.method === "GET") {
-    data = await prisma.manager.findMany();
+    let data = await prisma.manager.findMany({
+      select: {
+        name: true,
+        image: true,
+      },
+    });
+    res.status(200).json(data);
   }
+
+  // handling post requests
   if (req.method === "POST") {
-    data = await prisma.manager.create({});
+    await prisma.manager.create({
+      data: {
+        name,
+        username,
+        password: username,
+        image,
+      },
+    });
+
+    res.sendStatus(200).send("OK");
   }
-  res.json(data);
 }
