f440586eb719c0a5b66d0ddd92d440ce9a741241
Humboorgir <hardcraftap@gmail.com>
2023-06-28 15:20:30 +0330

    delete unnecessary api's and rewrite teachers api

--- pages/api/teachers.js
+++ pages/api/teachers.js
@@ -1,8 +1,60 @@
-import teacherModel from "@/models/teachers";
-import mongoConnect from "@/utils/mongoconnect";
+import prisma from "@/lib/prisma";
+
 export default async function handler(req, res) {
-  if (req.method !== "GET") return res.send("only get requests are allowed");
-  mongoConnect();
-  let data = await teacherModel.find({});
-  res.send(data);
+  // handling get requests
+  if (req.method === "GET") {
+    let data = await prisma.teacher.findMany({
+      select: {
+        id: true,
+        name: true,
+        image: true,
+      },
+    });
+    res.status(200).json(data);
+  }
+
+  // handling post requests
+  if (req.method === "POST") {
+    if (!req.body) return;
+    const { username, name } = req.body;
+    let image = String(req.body.image);
+
+    const data = await prisma.teacher.create({
+      data: {
+        name,
+        username,
+        password: username,
+        image,
+      },
+    });
+    res.status(200).json({ name: data.name, id: data.id, image: data.image });
+  }
+
+  if (req.method === "DELETE") {
+    if (!req.body) return;
+    const { id } = req.body;
+
+    await prisma.teacher.delete({
+      where: {
+        id,
+      },
+    });
+    return res.status(200).send("OK");
+  }
+
+  if (req.method === "PUT") {
+    if (!req.body) return;
+    const { username, name, id } = req.body;
+    await prisma.teacher.update({
+      where: {
+        id,
+      },
+      data: {
+        username,
+        name,
+      },
+    });
+
+    return res.status(200).send("OK");
+  }
 }
