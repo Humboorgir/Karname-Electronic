0f66f507aaafe8f357ae6395db55386ac77ff6f8
Humboorgir <hardcraftap@gmail.com>
2023-07-27 16:19:59 +0330

    Customize home/form component
    -change its wave svg and button color to red
    -change "Admin login" to "User login"

--- components/home/form.js
+++ components/home/form.js
@@ -1,8 +1,9 @@
-import { FaExclamationTriangle } from "react-icons/fa";
-// next-auth/client was renamed to next-auth/react in v4
 import { signIn } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
+
+import Image from "next/image";
+
 const Form = ({ setError }) => {
   const router = useRouter();
   useEffect(() => {
@@ -46,13 +47,13 @@ const Form = ({ setError }) => {
       method="POST"
       className="flex flex-col gap-8 border-[1px] border-gray-400 rounded-lg p-10
         shadow-md w-[350px] relative">
-      <img src="/wavesRed.svg" className="top-0 left-0 absolute rounded-lg"></img>
+      <img src="/waves.svg" className="top-0 left-0 absolute rounded-lg"></img>
       <div className="flex flex-row items-center justify-center gap-2">
-        <h1 className="bold text-white text-xl z-10">ورود نماینده</h1>
-        <FaExclamationTriangle className="h-[50px] z-10 text-yellow-400 text-[25px]"></FaExclamationTriangle>
+        <Image className="z-10" width={30} height={30} src="/logo.svg" alt="Logo"></Image>
+        <h1 className="bold text-white text-xl z-10">ورود کاربر</h1>
       </div>
       <div className="flex flex-col inputAnimationField">
-        <label htmlFor="username" className="text-lg z-10 mb-[5px]">
+        <label htmlFor="username" className="text-lg z-10 mb-[5px] mt-2">
           نام کاربری
         </label>
         <input
@@ -80,7 +81,7 @@ const Form = ({ setError }) => {
       <button
         onClick={handleSubmit}
         type="submit"
-        className="bg-red-500 rounded-3xl p-3 text-white z-10 clickAnimation">
+        className="bg-blue rounded-3xl p-3 text-white z-10 clickAnimation">
         ورود
       </button>
     </form>
