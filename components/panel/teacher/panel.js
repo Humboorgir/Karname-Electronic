07d2ee546ebdb3be9a73845d1cb16638f3f9ce68
Humboorgir <hardcraftap@gmail.com>
2023-07-29 01:55:45 +0330

    Paste student component from panel/admin to panel/teacher

--- components/panel/teacher/panel.js
+++ components/panel/teacher/panel.js
@@ -1,35 +1,13 @@
-import Link from "next/link";
-const Panel = () => {
+import Student from "@/components/panel/teacher/student";
+
+const Panel = ({ students }) => {
   return (
-    <div className="m-10 flex flex-row flex-wrap gap-5 justify-center items-center">
-      <Link
-        href="/panel/admin/students"
-        className="flex flex-col items-center justify-center gap-3 bg-blue text-white
-      h-[300px] w-[300px] rounded-lg shadow-2xl cursor-pointer scale-100 transition-transform ease-in-out
-       duration-500 hover:scale-105 hover:ease-in-out hover:transition-transform hover:duration-500
-       active:scale-95 active:ease-in-out active:transition-transform active:duration-300">
-        <img className="h-[200px]" src="/student.svg" />
-        <span className="text-[23px]">مدیریت دانش آموزان</span>
-      </Link>
-      <Link
-        href="/panel/admin/teachers"
-        className="flex flex-col items-center justify-center gap-3 bg-blue text-white
-      h-[300px] w-[300px] rounded-lg shadow-2xl cursor-pointer scale-100 transition-transform ease-in-out
-      duration-500 hover:scale-105 hover:ease-in-out hover:transition-transform hover:duration-500
-      active:scale-95 active:ease-in-out active:transition-transform active:duration-300">
-        <img className="h-[200px]" src="/teacher.svg" />
-        <span className="text-[23px]">مدیریت معلمان </span>
-      </Link>
-      <Link
-        href="/panel/admin/managers"
-        className="flex flex-col items-center justify-center gap-3 bg-blue text-white
-      h-[300px] w-[300px] rounded-lg shadow-2xl cursor-pointer scale-100 transition-transform ease-in-out
-      duration-500 hover:scale-105 hover:ease-in-out hover:transition-transform hover:duration-500
-      active:scale-95 active:ease-in-out active:transition-transform active:duration-300">
-        <img className="h-[200px]" src="/manager.svg" />
-        <span className="text-[23px]">مدیریت نمایندگان </span>
-      </Link>
-    </div>
+    <>
+      {students &&
+        students.map(({ name, image }) => {
+          return <Student name={name} image={image} />;
+        })}
+    </>
   );
 };
 
---
+++ components/panel/teacher/student.js
@@ -0,0 +1,25 @@
+import { FaTrash, FaEdit } from "react-icons/fa";
+const Student = ({ id, name, image, openModal }) => {
+  return (
+    <li
+      key={id}
+      className="flex flex-row justify-end items-center gap-3 border-b-2 border-neutral-300
+  pb-3 px-5">
+      <span className="mr-auto flex flex-row items-center gap-2">
+        <span className="relative">
+          <FaEdit onClick={() => openModal("edit", id)} className="text-gray-800 mb-[2px] cursor-pointer" />
+        </span>
+        <span className>
+          <FaTrash
+            onClick={() => openModal("delete", id)}
+            className="text-gray-800 relative cursor-pointer"
+          />
+        </span>
+      </span>
+      <span>{name}</span>
+      <img className="h-[40px]" src={`/defaultStudent${image}.svg`} />
+    </li>
+  );
+};
+
+export default Student;
