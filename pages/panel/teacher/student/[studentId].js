4e64b927597c59cde1a2ccb453f3697f95a226fb
Humboorgir <hardcraftap@gmail.com>
2023-07-29 19:05:36 +0330

    Design basic title, navbar, footer and ... in panel/teacher/student/[studentId]

--- pages/panel/teacher/student/[studentId].js
+++ pages/panel/teacher/student/[studentId].js
@@ -1,4 +1,6 @@
 import Head from "@/components/head";
+import Navbar from "@/components/panel/teacher/navbar";
+import Footer from "@/components/footer";
 
 import { useRouter } from "next/router";
 
@@ -7,7 +9,14 @@ const Student = () => {
   return (
     <>
       <Head page="ارسال نمرات" />
-      <div>student id: {router.query.studentId}</div>
+      <div className="flex flex-col justify-between">
+        <Navbar />
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl mb-1">{router.query.studentId}</h1>
+          <p className="text-sm text-slate-700">برای ثبت نمره دانش آموز یکی از دروس زیر را انتخاب کنید.</p>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 };
