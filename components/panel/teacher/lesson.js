10aeffe934d2b5ba3fd786b185bdbabeb9f792e1
Humboorgir <hardcraftap@gmail.com>
2023-07-30 17:16:57 +0330

    Change Lesson component parent element from button to next.js Link

--- components/panel/teacher/lesson.js
+++ components/panel/teacher/lesson.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const Lesson = ({ title }) => {
   return (
-    <button
+    <Link
       className="bg-gradient-to-br from-sky-600 to-sky-400 text-white 
     text-2xl bold rounded-md grid place-items-center w-28 h-24 hover:cursor-pointer
     scale-100 hover:scale-110 active:scale-95 transition-all duration-[400ms]">
       {title}
-    </button>
+    </Link>
   );
 };
 
