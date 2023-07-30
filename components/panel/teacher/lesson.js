54ea6133635ce0b168eca1c9f59bcda5b627bde8
Humboorgir <hardcraftap@gmail.com>
2023-07-30 21:58:19 +0330

    Update database schema

--- components/panel/teacher/lesson.js
+++ components/panel/teacher/lesson.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
-const Lesson = ({ title }) => {
+const Lesson = ({ title, url }) => {
   return (
     <Link
       className="bg-gradient-to-br from-sky-600 to-sky-400 text-white 
     text-2xl bold rounded-md grid place-items-center w-28 h-24 hover:cursor-pointer
-    scale-100 hover:scale-110 active:scale-95 transition-all duration-[400ms]">
+    scale-100 hover:scale-110 active:scale-95 transition-all duration-[400ms]"
+      href={url}>
       {title}
     </Link>
   );
