4d06dc2e0288d76393fe9216cd16fae798955fab
Humboorgir <hardcraftap@gmail.com>
2023-07-30 22:03:52 +0330

    Make footer component stick at the bottom of the page

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer
       className="flex flex-row justify-center items-center py-8 border-t-[3px]
-    text-center w-full">
+    text-center w-full mt-auto ">
       <span>Copyright &copy; 2023 Karname Electronic, All rights reserved.</span>
     </footer>
   );
