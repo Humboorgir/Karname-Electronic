251d80f98ddd2160556d6affdebe75e47d9fe50d
Humboorgir <hardcraftap@gmail.com>
2023-07-31 23:57:35 +0330

    Create MarkInput component

---
+++ components/panel/teacher/student/markinput.js
@@ -0,0 +1,16 @@
+const MarkInput = ({ text, id }) => {
+  return (
+    <div className="flex items-center justify-start gap-2 md:gap-4 w-36">
+      <input
+        className="p-2 px-4 max-w-[4rem] rounded-md focus:outline-none border border-neutral-400"
+        name={id}
+        placeholder="۲۰"
+        id={id}></input>
+      <label className="text-lg font-bold" for={id}>
+        {text}
+      </label>
+    </div>
+  );
+};
+
+export default MarkInput;
