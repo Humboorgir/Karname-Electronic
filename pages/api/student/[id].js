07aa3d931fd17f79e5158d7ab47ef6171191632e
Humboorgir <hardcraftap@gmail.com>
2023-08-01 16:50:24 +0330

    Handle post requests sent to /api/student/[studentId]

--- pages/api/student/[id].js
+++ pages/api/student/[id].js
@@ -5,14 +5,43 @@ export default async function handler(req, res) {
     let { id } = req.query;
     id = Number(id);
 
-    if (!id) return res.status(400).send("Bad request");
+    if (!id) return res.status(400).send({ data: "Bad request" });
 
     let data = await prisma.student.findUnique({
       where: {
         id,
       },
     });
-    if (!data) return res.status(404).send("Not found");
+    if (!data) return res.status(404).send({ data: "Not found" });
     res.status(200).json(data);
+  }
+
+  if (req.method === "POST") {
+    let { id } = req.query;
+    id = Number(id);
+
+    if (!id) return res.status(400).json({ data: "Bad request" });
+
+    let data = req.body;
+    data = await JSON.parse(data);
+
+    let createdReport = await prisma.student.update({
+      where: {
+        id,
+      },
+      data: {
+        reports: {
+          create: {
+            ...data,
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      data: {
+        ...createdReport,
+      },
+    });
   }
 }
