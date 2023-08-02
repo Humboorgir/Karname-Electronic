465f42ad82068bb3eb79bd2a8d0a8844b66a5803
Humboorgir <hardcraftap@gmail.com>
2023-08-02 17:16:18 +0330

    Create NoDataImage component

---
+++ components/panel/student/nodataimage.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const Image_ = () => {
+  return <Image className="mr-8" src={`/nodata.svg`} height={200} width={200} alt={student.name} />;
+};
+
+export default Image_;
