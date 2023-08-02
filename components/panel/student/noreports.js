c09185fce4285136749dedd51f7119e79bb950b5
Humboorgir <hardcraftap@gmail.com>
2023-08-02 17:17:54 +0330

    Create NoReports component (panel/student/noreports)

---
+++ components/panel/student/noreports.js
@@ -0,0 +1,17 @@
+import NoDataImage from "@/components/panel/student/nodataimage";
+
+const NoReports = () => {
+  return (
+    <div className="mt-16 flex flex-col items-center justify-center text-center">
+      <NoDataImage />
+      <h2 className="text-2xl bold mt-4 px-2">کارنامه ای برای شما ثبت نشده است</h2>
+      <p className="text-sm text-slate-700 max-w-md mt-1.5 px-2">
+        ثبت کردن کارنامه دانش آموزان پروسه ای زمان بر است, در صورتی که از ثبت شده بودن کارنامه خود مطمئن هستید
+        به نماینده های مدرسه خبر دهید
+      </p>
+      <h4 className="text-sm text-slate-700 mt-1.5">کد شناسایی دانش آموز: {global.student.id}</h4>
+    </div>
+  );
+};
+
+export default NoReports;
