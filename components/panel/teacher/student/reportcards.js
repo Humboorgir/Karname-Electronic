fe1ec0addbee1f32889c755a25fdfb967162f54c
Humboorgir <hardcraftap@gmail.com>
2023-09-08 21:30:05 +0330

    Center report cards on /panel/teacher/student/[id]

--- components/panel/teacher/student/reportcards.js
+++ components/panel/teacher/student/reportcards.js
@@ -2,7 +2,7 @@ import ReportCard from "@/components/panel/teacher/student/reportcard";
 
 const ReportCards = ({ reports }) => {
   return (
-    <div className="flex flex-col gap-8 mt-16">
+    <div className="flex flex-col gap-8 mt-16 mx-auto">
       {reports.map((report) => {
         return <ReportCard report={report} />;
       })}
