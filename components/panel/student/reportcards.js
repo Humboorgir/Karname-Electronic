e6b0e94069c42f5324ccfd23573bc967f949f0fe
Humboorgir <hardcraftap@gmail.com>
2023-09-09 22:40:27 +0330

    /panel/student: Add some space between reportcards and footer

--- components/panel/student/reportcards.js
+++ components/panel/student/reportcards.js
@@ -2,7 +2,7 @@ import ReportCard from "@/components/panel/student/reportcard";
 
 const ReportCards = ({ reports }) => {
   return (
-    <section id="reportcards" className="flex flex-col gap-16 mt-[180px] px-2.5">
+    <section id="reportcards" className="flex flex-col gap-16 mt-[180px] px-2.5 mb-[100px]">
       {reports.map((report) => {
         return <ReportCard report={report} />;
       })}
